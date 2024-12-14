b135d3f111dc86e538557fcb5fbc4cda708589f5
Fukue <tignatowicz2009@gmail.com>
2024-12-14 01:54:11 +0100

    v0.3

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Cclicker
 
-Cel aplikacji: Twoim celem jest zdobyć jak najwięcej punktów klikając.
+Simple game about clicking the button.
 
-Zasada działania: klikając w przycisk dostajesz punkty.
+For clicking the button you gain "clicks" that you can spend on upgrades to get clicks faster.
+For 25000 clicks you can unlock the button that will automatically convert every 100 clicks to 1 click per button press more.
+After getting to 5000 clicks you unlock a way to gamble with clicks so getting them is easier and more fun.
 
-Mechanizm interakcji: Klikanie. Im więcej klikasz, tym więcej zdobywasz punktów.
+You can gamble with clicks by:
+- throwing a coin -> by winning you get 2x bet.
+- rolling the dice -> by winning you get 6x bet.
--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 
-let totalCount = 0
+let totalCount = 5000
 let iteration = 1
 
 let i = 0
+document.getElementById("pixels").innerHTML = "pixels: " + totalCount;
+ 
 
 
 function count(){
     
     document.getElementById("output").innerHTML = totalCount = totalCount + iteration;
+  
 
     if(i){
-    document.getElementById("fbUpgrade").innerHTML = (totalCount - totalCount%100 )/ 100 + "/Click"
+    document.getElementById("fbUpgrade").innerHTML = (totalCount - totalCount%100 )/ 100 + "/Click" 
     document.getElementById("fbPrice").innerHTML = (totalCount - (totalCount%100))
     }
+
+    
 }
 
-
-
-function upgrade(tier,price){
-    
-
+function upgrade(tier,price,t){
     if(totalCount >= price){
-        
-
         totalCount = totalCount - price
         iteration = iteration + tier
         
-        
-
         document.getElementById("cPerClick").innerHTML = iteration
         document.getElementById("output").innerHTML = totalCount
-        
+            
     }
     else{
         alert("you can't afford that" )
+
     }
-
-
 }
+
+
 
 function finalButton(price){
 
@@ -57,8 +55,74 @@ function finalButton(price){
         totalCount = totalCount - (totalCount - (totalCount%100))
         document.getElementById("fbUpgrade").innerHTML = (totalCount - totalCount%100 )/ 100 + "/Click"
         document.getElementById("fbPrice").innerHTML = (totalCount - (totalCount%100))
-
     }
     document.getElementById("cPerClick").innerHTML = iteration
     document.getElementById("output").innerHTML = totalCount
+}
+
+function cf(){
+
+    let b = document.getElementsByName("wawa")[0].value
+    
+    if (b > totalCount){
+        alert("you can't afford that!")
+        return 0;
+    }
+    let a = prompt("[H] - heads | [T] - tails")
+    let c = 0
+
+    switch(a){
+        case 'h': c = 'heads'; break;
+        case 't': c = 'tails'; break;
+        case 'H': c = 'heads'; break;
+        default: console.log("a"); c = 'tails'; break;
+    }
+
+    let coin = Math.floor(Math.random() * 2 )
+    switch(coin){
+        case 0: coin = 'heads';break;
+        case 1: coin = 'tails';break;
+    }
+    console.log(coin)
+
+    if(coin == c){
+        document.getElementById('result').innerHTML = 'the coin landed on... <br> ' + coin + '<br><br> You win!';
+        totalCount = (totalCount + 1 * b)
+    }
+    else{
+        document.getElementById('result').innerHTML = 'the coin landed on... <br> ' + coin + '<br><br> You lose!';
+        totalCount -= b
+    }
+    document.getElementById("pixels").innerHTML = "pixels: " + totalCount;
+    console.log(totalCount)
+}
+
+function dr(){
+    
+    let b = document.getElementsByName("wawa")[0].value
+    
+    if (b > totalCount){
+        alert("you can't afford that!")
+        return 0;
+    }
+
+    let a = parseInt(prompt("Select number [1 - 6]"))
+    
+
+
+
+    let dice = Math.floor(Math.random() * 6 ) + 1
+
+
+
+    if(dice == a){
+        document.getElementById('result').innerHTML = 'the coin landed on... <br> ' + dice + '<br><br> You win!';
+        totalCount = totalCount + (6 * b)
+    }
+    else{
+        document.getElementById('result').innerHTML = 'the coin landed on... <br> ' + dice + '<br><br> You lose!';
+        totalCount -= b
+    }
+    document.getElementById("pixels").innerHTML = "pixels: " + totalCount;
+    console.log(totalCount)
 }
